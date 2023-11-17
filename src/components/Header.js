299641aa55e2e56f63b0b2b79aa77ee80bac7bc9
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2023-11-16 22:23:21 -0800

    added header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../styles/Header.module.css';
 
 export default function Header() {
     return (
-        <header>
-            <h1>Jammming</h1>
+        <header className={styles.header}>
+            <h1>Ja<span className={styles.mmm}>mmm</span>ing</h1>
         </header>
     )
 }
