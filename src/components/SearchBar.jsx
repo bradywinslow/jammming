d9853814b3f627ed42d29cb47a6fa844e337f6b7
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2023-12-05 22:26:39 -0800

    added state to clear SearchBar input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/SearchBar.module.css';
 
 export default function SearchBar() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleUserInput = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const resetInputField = () => {
+        setInputValue('');
+    };
+
     return (
         <search className={styles.searchDiv}>
             <form className={styles.searchContainer}>
-                <input className={styles.searchInput}></input>
+                <input type='text' value={inputValue} className={styles.searchInput} onChange={handleUserInput}></input>
             </form>
-            <button className={styles.searchButton}>Search</button>
+            <button onClick={resetInputField} className={styles.searchButton}>Search</button>
         </search>
     )
 }
