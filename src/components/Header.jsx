945da1a2c7f034afcd17403f596bde6b22d144a7
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-04-07 13:08:48 -0700

    add icons to title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from '../styles/Header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMusic } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     return (
         <div className={styles.header}>
-            <h1>Ja<span className={styles.mmm}>mmm</span>ing</h1>
+            <h1><FontAwesomeIcon icon={faMusic} className={styles.faMusic} /> Jammming <FontAwesomeIcon icon={faMusic} className={styles.faMusic} /></h1>
         </div>
     )
 }
