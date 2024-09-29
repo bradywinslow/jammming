a9c70dd9e3dbd7ac61a2252f5944f13b9c0084f9
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-09-29 00:21:07 -0700

    add responsiveness to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ export default function Header() {
         <Flex justify='center'>
             <Stack>
                 <Center gap={4} mt='6' mb='45'>
-                    <Icon as={FaHeadphones} boxSize={['2rem', '3rem']} color='#B28CE3'/>
-                    <Heading as='h1' size='2xl'><NavLink to='/'>Jammming</NavLink></Heading>
-                    <Icon as={FaHeadphones} boxSize='3rem' color='#B28CE3' />
+                    <Icon as={FaHeadphones} boxSize={['30px', '40px', '50px']} color='#B28CE3'/>
+                        <Heading as='h1' size={['xl', '2xl']}><NavLink to='/'>Jammming</NavLink></Heading>
+                    <Icon as={FaHeadphones} boxSize={['30px', '40px', '50px']} color='#B28CE3' />
                 </Center>
                 <Center>
-                    <Heading as='h2' size='md' mt='-2rem' mb='10'>
+                    <Heading as='h2' size={['xs', 'sm', 'md']} mt='-2rem' mb='10'>
                         A Spotify Playlist Builder
                     </Heading>
                 </Center>
